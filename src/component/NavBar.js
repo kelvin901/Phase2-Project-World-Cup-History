06f25a3cbdbab232d3a9e494ff57c48c8e88d9a3
kelvin901 <wawerukelvin2019@gmail.com>
2023-05-05 13:49:58 +0300

    Added some info

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return(
     <div className="navbar navbar-expand-lg ">
-      <a className="navbar-brand">World's Cup History</a>
+      <a className="navbar-brand">World Cup History</a>
       <div>
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
